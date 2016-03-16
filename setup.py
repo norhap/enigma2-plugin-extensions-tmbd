79023aaf7c7bda9ae30a5bc65e798a7a9f4a3143
Dima73 <Dima-73@inbox.lv>
2016-03-16 18:24:34 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,8 @@ setup(name = 'enigma2-plugin-extensions-tmbd',
 		version='6.9',
 		author='Dimitrij',
 		author_email='dima-73@inbox.lv',
-		package_dir = {'Extensions.TMBD': 'src'},
-		packages=['Extensions.TMBD'],
-		py_modules = ['profile', 'tmdb3'],
+		packages=['Extensions.TMBD.src', 'Extensions.TMBD.profile', 'Extensions.TMBD.tmdb3'],
 		package_data={'Extensions.TMBD': ['*.png', 'ir/*.png']},
 		description = 'Search the internet bases themoviedb.org/kinopoisk.ru',
 		cmdclass = setup_translate.cmdclass,
 	)
-
